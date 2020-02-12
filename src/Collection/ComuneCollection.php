bf2902f4d659d97dfbb38b681f4007b991b54c68
Pablo Largo Mohedano <devnix.code@gmail.com>
2020-02-12 18:14:33 +0100

    ArrayCollections and update of readme

---
+++ src/Collection/ComuneCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Devnix\BelfioreCode\Collection;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+
+class ComuneCollection extends ArrayCollection
+{
+    protected const JSON_PATH = __DIR__.'/../../dist/cities.json';
+
+    public function __construct(array $elements = null)
+    {
+        if (is_null($elements)) {
+            $serializer = new Serializer([], [new JsonEncoder()]);
+            $elements = $serializer->decode(ErrorHandler::call('file_get_contents', self::JSON_PATH), 'json');
+        }
+
+        parent::__construct($elements);
+    }
+}
